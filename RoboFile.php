9aa02ac688ac0a914e5b028e754ecc4760800713
Ivan <devskyfly@gmail.com>
2020-02-14 15:58:26 +0300

    Update RoboFile.php.

--- RoboFile.php
+++ RoboFile.php
@@ -1,5 +1,16 @@
 <?php
-require __DIR__."/vendor/autoload.php";
+global $yiiApp;
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+
+// загрузка конфигурации приложения
+$config = require __DIR__ . '/tests/app/config/console.php';
+
+// создание и конфигурация приложения, а также вызов метода для обработки входящего запроса
+$yiiApp = (new yii\console\Application($config));
 
 use devskyfly\robocmd\DevTestTrait;
 
